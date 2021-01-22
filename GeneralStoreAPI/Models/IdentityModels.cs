0d5b9752680e2b5f666f2b6c1777bcee4192f7ec
joemacko <joemacko58@gmail.com>
2021-01-21 20:42:23 -0500

    Created Customer, Product, and Transaction classes (also known as models or tables). Completed properties for those classes. Added DbSet to IdentityModels class. Enabled migrations, added InitialMigration, and updated database. Setup connection string.

--- GeneralStoreAPI/Models/IdentityModels.cs
+++ GeneralStoreAPI/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -29,5 +30,11 @@ namespace GeneralStoreAPI.Models
         {
             return new ApplicationDbContext();
         }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Transaction> Transactions { get; set; }
     }
 }
---
+++ GeneralStoreAPI/Models/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStoreAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public string SKU { get; set; }
+
+        public string Name { get; set; }
+
+        public double Cost { get; set; }
+
+        public int NumberInInventory { get; set; }
+
+        public bool IsInStock
+        {
+            get
+            {
+                int numberInStock = NumberInInventory;
+                if (numberInStock > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
