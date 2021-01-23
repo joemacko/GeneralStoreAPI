e533f040c791ceae9ad9195f04a7cf1a718d9b58
joemacko <joemacko58@gmail.com>
2021-01-23 11:21:34 -0500

    Added CRUD functionality to CustomerController

--- GeneralStoreAPI/Controllers/CustomerController.cs
+++ GeneralStoreAPI/Controllers/CustomerController.cs
@@ -1,6 +1,7 @@
 ﻿using GeneralStoreAPI.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -31,6 +32,81 @@ namespace GeneralStoreAPI.Controllers
             }
 
             return BadRequest(ModelState);
+        }
+
+        // GET ALL
+        // api/Customer
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAllCustomers()
+        {
+            List<Customer> customers = await _context.Customers.ToListAsync();
+            return Ok(customers);
+        }
+
+        // GET BY ID
+        // api/Restaurant/{id}
+        [HttpGet]
+        public async Task<IHttpActionResult> GetCustomerById([FromUri] int id)
+        {
+            Customer customer = await _context.Customers.FindAsync(id);
+
+            if(customer != null)
+            {
+                return Ok(customer);
+            }
+
+            return NotFound();
+        }
+
+        // PUT (update)
+        // api/Customer/{id}
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateCustomer([FromUri] int id, [FromBody] Customer updatedCustomer)
+        {
+            // Check if ids match
+            if (id != updatedCustomer?.Id)
+            {
+                return BadRequest("Ids do not match");
+            }
+
+            // Check the ModelState
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            // Find customer in database
+            Customer customer = await _context.Customers.FindAsync(id);
+
+            // If customer doesn't exist, do something
+            if (customer is null)
+                return NotFound();
+
+            // Update the properties
+            customer.FirstName = updatedCustomer.FirstName;
+            customer.LastName = updatedCustomer.LastName;
+
+            // Save the changes
+            await _context.SaveChangesAsync();
+            return Ok("The customer was updated!");
+        }
+
+        // DELETE (delete)
+        // api/Customer/{id}
+        [HttpDelete]
+        public async Task<IHttpActionResult> DeleteCustomer([FromUri] int id)
+        {
+            Customer customer = await _context.Customers.FindAsync(id);
+
+            if (customer is null)
+                return NotFound();
+
+            _context.Customers.Remove(customer);
+
+            if (await _context.SaveChangesAsync() == 1)
+            {
+                return Ok("The customer was deleted");
+            }
+
+            return InternalServerError();
         }
     }
 }
---
+++ GeneralStoreAPI/Migrations/202101231535211_SecondMigration.cs
@@ -0,0 +1,32 @@
+﻿namespace GeneralStoreAPI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SecondMigration : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Transactions", "ProductSKU", "dbo.Products");
+            DropIndex("dbo.Transactions", new[] { "ProductSKU" });
+            AlterColumn("dbo.Customers", "FirstName", c => c.String(nullable: false));
+            AlterColumn("dbo.Customers", "LastName", c => c.String(nullable: false));
+            AlterColumn("dbo.Products", "Name", c => c.String(nullable: false));
+            AlterColumn("dbo.Transactions", "ProductSKU", c => c.String(nullable: false, maxLength: 128));
+            CreateIndex("dbo.Transactions", "ProductSKU");
+            AddForeignKey("dbo.Transactions", "ProductSKU", "dbo.Products", "SKU", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Transactions", "ProductSKU", "dbo.Products");
+            DropIndex("dbo.Transactions", new[] { "ProductSKU" });
+            AlterColumn("dbo.Transactions", "ProductSKU", c => c.String(maxLength: 128));
+            AlterColumn("dbo.Products", "Name", c => c.String());
+            AlterColumn("dbo.Customers", "LastName", c => c.String());
+            AlterColumn("dbo.Customers", "FirstName", c => c.String());
+            CreateIndex("dbo.Transactions", "ProductSKU");
+            AddForeignKey("dbo.Transactions", "ProductSKU", "dbo.Products", "SKU");
+        }
+    }
+}
