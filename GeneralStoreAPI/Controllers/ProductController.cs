b422e61c64b5cdd18128fedb927752ffbc24aefa
joemacko <joemacko58@gmail.com>
2021-01-23 21:49:46 -0500

    Added all CRUD functionality to ProductController

--- GeneralStoreAPI/Controllers/ProductController.cs
+++ GeneralStoreAPI/Controllers/ProductController.cs
@@ -10,79 +10,75 @@ using System.Web.Http;
 
 namespace GeneralStoreAPI.Controllers
 {
-    public class CustomerController : ApiController
+    public class ProductController : ApiController
     {
         private readonly ApplicationDbContext _context = new ApplicationDbContext();
 
         // POST (Create)
-        // api/Customer
+        // api/Product
         [HttpPost]
-        public async Task<IHttpActionResult> CreateCustomer([FromBody] Customer model)
+        public async Task<IHttpActionResult> CreateProduct([FromBody] Product model)
         {
             if (model is null)
-            {
                 return BadRequest("The request body cannot be empty");
-            }
 
             if (ModelState.IsValid)
             {
-                _context.Customers.Add(model);
+                _context.Products.Add(model);
                 int changeCount = await _context.SaveChangesAsync();
-                return Ok("The customer was successfully created");
+                return Ok("The product was successfully created!");
             }
 
             return BadRequest(ModelState);
         }
 
         // GET ALL
-        // api/Customer
+        // api/Product
         [HttpGet]
-        public async Task<IHttpActionResult> GetAllCustomers()
+        public async Task<IHttpActionResult> GetAllProducts()
         {
-            List<Customer> customers = await _context.Customers.ToListAsync();
-            return Ok(customers);
+            List<Product> products = await _context.Products.ToListAsync();
+            return Ok(products);
         }
 
         // GET BY ID
-        // api/Restaurant/{id}
+        // api/Product/{id}
         [HttpGet]
-        public async Task<IHttpActionResult> GetCustomerById([FromUri] int id)
+        public async Task<IHttpActionResult> GetProductById([FromUri] string sku)
         {
-            Customer customer = await _context.Customers.FindAsync(id);
+            Product product = await _context.Products.FindAsync(sku);
 
-            if(customer != null)
-            {
-                return Ok(customer);
-            }
+            if (product != null)
+                return Ok(product);
 
             return NotFound();
         }
 
         // PUT (Update)
-        // api/Customer/{id}
+        // api/Product/{id}
         [HttpPut]
-        public async Task<IHttpActionResult> UpdateCustomer([FromUri] int id, [FromBody] Customer updatedCustomer)
+        public async Task<IHttpActionResult> UpdateProduct([FromUri] string sku, [FromBody] Product updatedProduct)
         {
             // Check if ids match
-            if (id != updatedCustomer?.Id)
-            {
+            if (sku != updatedProduct?.SKU)
                 return BadRequest("Ids do not match");
-            }
 
             // Check the ModelState
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
             // Find customer in database
-            Customer customer = await _context.Customers.FindAsync(id);
+            Product product = await _context.Products.FindAsync(sku);
 
             // If customer doesn't exist, do something
-            if (customer is null)
+            if (product is null)
                 return NotFound();
 
             // Update the properties
-            customer.FirstName = updatedCustomer.FirstName;
-            customer.LastName = updatedCustomer.LastName;
+            // product.SKU = updatedProduct.SKU; - can't update this as of now (ids won't match then)
+            product.Name = updatedProduct.Name;
+            product.Cost = updatedProduct.Cost;
+            product.NumberInInventory = updatedProduct.NumberInInventory;
 
             // Save the changes
             await _context.SaveChangesAsync();
@@ -90,21 +86,19 @@ namespace GeneralStoreAPI.Controllers
         }
 
         // DELETE (Delete)
-        // api/Customer/{id}
+        // api/Product/{id}
         [HttpDelete]
-        public async Task<IHttpActionResult> DeleteCustomer([FromUri] int id)
+        public async Task<IHttpActionResult> DeleteProduct([FromUri] string sku)
         {
-            Customer customer = await _context.Customers.FindAsync(id);
+            Product product = await _context.Products.FindAsync(sku);
 
-            if (customer is null)
+            if (product is null)
                 return NotFound();
 
-            _context.Customers.Remove(customer);
+            _context.Products.Remove(product);
 
             if (await _context.SaveChangesAsync() == 1)
-            {
                 return Ok("The customer was deleted");
-            }
 
             return InternalServerError();
         }
